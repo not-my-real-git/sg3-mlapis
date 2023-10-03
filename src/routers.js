ce1919fe5cf1cf2450e89a1ecd084672743d7e6c
Noshe <nosheavan@gmail.com>
2023-10-04 03:07:37 +0700

    mlapis-cr first commit

---
+++ src/routers.js
@@ -0,0 +1,28 @@
+const { predict } = require('./methods')
+
+const routers = [
+  {
+    method: 'POST',
+    path: '/predict',
+    handler: predict.Predict,
+    options: {
+      payload: {
+        output: 'data',
+        parse: true,
+        multipart: true
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/storage-test',
+    handler: predict.StorageTest
+  },
+  {
+    method: 'GET',
+    path: '/logs/show',
+    handler: predict.LogsRetrieve
+  }
+]
+
+module.exports = { routers }
